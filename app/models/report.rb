398c8160f29c895e26f47c23f93db635ae3cc645
Nícolas Iensen <nicolas@engage.is>
2013-08-31 18:23:41 -0300

    Remove swp files and fix report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,4 @@
 class Report < ActiveRecord::Base
   attr_accessible :path_id, :rating, :user_id
-  validates :path_id, :rating, :user_id
+  validates :path_id, :rating, :user_id, presence: true
 end
